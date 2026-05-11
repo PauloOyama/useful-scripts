557f1bbd92097e398a494594f8f41a8711053c17
Paulo Oyama <papaloyama@gmail.com>
2026-05-11 09:52:28 -0300

    Add create folder functions

--- mail_organizer.py
+++ mail_organizer.py
@@ -1,8 +1,9 @@
-from imap_tools import MailBox
+from imap_tools import AND, MailBox
 import os
 from dotenv import load_dotenv
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import Tuple
 
 # Configurar logging com datetime
 logging.basicConfig(
@@ -10,6 +11,31 @@ logging.basicConfig(
     format='%(asctime)s - RUN - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
+
+
+
+def create_folder(mailbox:MailBox) -> bool:
+    try:
+        if not mailbox.folder.exists("PAGAMENTOS"):
+            _,status_code = mailbox.folder.create('PAGAMENTOS')
+            logger.info(f"Creation Folder PAGAMENTOS {status_code}")
+
+        if not mailbox.folder.exists("NOTÍCIAS"):
+            _,status_code = mailbox.folder.create('NOTÍCIAS')
+            logger.info(f"Creation Folder NOTÍCIAS {status_code}")
+
+        if not mailbox.folder.exists("INVESTIMENTOS"):
+            _,status_code = mailbox.folder.create('INVESTIMENTOS')
+            logger.info(f"Creation Folder INVESTIMENTOS {status_code}")
+
+    except Exception as e:
+         logger.warning("Error: Creating Folders!")
+         logger.warning("Terminating the program")
+         raise Exception(e)
+
+    logger.info("All folder created")
+    return True
+
 
 logger = logging.getLogger(__name__)
 
@@ -24,5 +50,14 @@ logger.info(f"Password carregada com sucesso")
 
 # print(email, password)
 with MailBox('imap.gmail.com').login(username=email, password=password) as mailbox:
-    for msg in mailbox.fetch():
+    current_folder = mailbox.folder.get()
+    logger.info(f"Current Folder - {current_folder}")
+    
+    
+    create_folder(mailbox=mailbox)
+    
+    date_filtered = datetime.now() - timedelta(days=60)
+    
+    print(date_filtered.strftime("%d-%b-%Y"))
+    for msg in mailbox.fetch(AND(date_gte=date_filtered.date())):
         print(msg.date, msg.subject, len(msg.text or msg.html))
