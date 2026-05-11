416bcf54b20aaabd1fcd7dea582b14c71e7bd9a1
Paulo Oyama <papaloyama@gmail.com>
2026-05-11 13:37:08 -0300

    add filters

--- mail_organizer.py
+++ mail_organizer.py
@@ -28,6 +28,18 @@ def create_folder(mailbox:MailBox) -> bool:
             _,status_code = mailbox.folder.create('INVESTIMENTOS')
             logger.info(f"Creation Folder INVESTIMENTOS {status_code}")
 
+        if not mailbox.folder.exists("TI VAGAS"):
+            _,status_code = mailbox.folder.create('TI VAGAS')
+            logger.info(f"Creation Folder TI VAGAS {status_code}")
+
+        if not mailbox.folder.exists("ENTRETENIMENTO"):
+            _,status_code = mailbox.folder.create('ENTRETENIMENTO')
+            logger.info(f"Creation Folder ENTRETENIMENTO {status_code}")
+
+        if not mailbox.folder.exists("TI ARTIGOS"):
+            _,status_code = mailbox.folder.create('TI ARTIGOS')
+            logger.info(f"Creation Folder TI ARTIGOS {status_code}")
+
     except Exception as e:
          logger.warning("Error: Creating Folders!")
          logger.warning("Terminating the program")
@@ -35,6 +47,19 @@ def create_folder(mailbox:MailBox) -> bool:
 
     logger.info("All folder created")
     return True
+
+
+def email2move(sender:str,topic:str)->None:
+    try:
+            logging.info(f"Searching emails from {sender}")
+            uids2move = mailbox.uids(AND(from_=sender), charset='utf8')
+
+            logging.info(f"Moving emails from {sender} to {topic} - {len(uids2move)} Founded!")
+            mailbox.move(uids2move, topic, chunks=100)
+    except Exception as e:
+            logging.warning("Error: When moving file...")
+
+    logging.info(f"All email from {sender} to {topic} moved!")
 
 
 logger = logging.getLogger(__name__)
@@ -58,6 +83,19 @@ with MailBox('imap.gmail.com').login(username=email, password=password) as mailb
     
     date_filtered = datetime.now() - timedelta(days=60)
     
-    print(date_filtered.strftime("%d-%b-%Y"))
-    for msg in mailbox.fetch(AND(date_gte=date_filtered.date())):
-        print(msg.date, msg.subject, len(msg.text or msg.html))
+    logging.info(f"Time Filtered - AFTER {date_filtered.strftime("%d-%b-%Y")}")
+
+    email2move(sender='noreply@medium.com',topic='NOTÍCIAS')
+    email2move(sender='',topic='NOTÍCIAS')
+    email2move(sender='tesourodireto@b3.com.br',topic='INVESTIMENTOS')
+    email2move(sender='todomundo@nubank.com.br',topic='INVESTIMENTOS')
+    email2move(sender='contato@emkt.b3.com.br',topic='INVESTIMENTOS')
+    email2move(sender='relacionamento@kineainvestimentos.com.br',topic='INVESTIMENTOS')
+    email2move(sender='jobalerts-noreply@linkedin.com',topic='TI VAGAS')
+    email2move(sender='.jobs2web.',topic='TI VAGAS')
+    email2move(sender='glassdoor',topic='TI VAGAS')
+    email2move(sender='tiktok',topic='ENTRETENIMENTO')
+    email2move(sender='datatalks',topic='TI ARTIGOS')
+    email2move(sender='filipedeschamps',topic='TI ARTIGOS')
+    # for msg in mailbox.fetch(AND(date_gte=date_filtered.date())):
+    #     print(msg.date, msg.subject, len(msg.text or msg.html))
